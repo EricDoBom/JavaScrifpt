87cf7c5a264ef5dac87c7ac971d5703424dee8fa
Eric Oliveira <163408147+EricDoBom@users.noreply.github.com>
2024-08-14 07:48:04 -0300

    Add files via upload

---
+++ atividadejavascript.js
@@ -0,0 +1,13 @@
+console.log("Atividade em Javascript")
+
+const nome = "Eric";
+const idade = 17;
+const email = "oliveireric907@gmail.com";
+const endereço = "Rua Aurea da Silva Senff n145";
+const sexo = "Masculino";
+const profissao = "estudante";
+
+console.log(nome)
+console.log(idade)
+console.log()
+
